5b89c0508e8e2a5a0df3d1c1c4e9ba6cbef876e5
Stacy Johns <stacykjohns@icloud.com>
2019-11-14 17:18:24 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,4 +15,12 @@ class Customer
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
+
+  def meals
+    Meals.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  end
 end
